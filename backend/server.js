65c9ea8ed6c1871a12c5cc31e75a927fc2161ee3
sriram1015 <sriramvenkatachalpath2601@gmail.com>
2025-08-23 00:13:16 +0530

    versel url update

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "military-asset-management-system-weld.vercel.app",
   credentials: true
 }));
 
